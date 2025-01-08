859ad71972e74c0263b538b2ede93ac78ba9e0b6
Robbo <robbo.clancy@gmail.com>
2025-01-08 22:32:30 +1000

    nushell yolo

---
+++ env/.config/nvim/lua/options.lua
@@ -0,0 +1,23 @@
+vim.opt.cursorline = true
+vim.opt.inccommand = 'split'
+vim.opt.cmdheight = 0
+
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.relativenumber = false
+vim.opt.number = false
+
+vim.opt.wrap = true
+
+vim.opt.pumheight = 6
+vim.opt.pumwidth = 18
+vim.opt.pumblend = 15
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.swapfile = false
+vim.opt.undofile = true
---
+++ env/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,5 @@
+require("oil").setup({
+	columns = {}
+})
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
