cb2fd7523b46e9f6b341eadc4e1edeb15a59af7b
Robbo <robbo.clancy@gmail.com>
2025-05-07 22:26:59 +1000

    added live grep and echos for when using old binds

--- env/.config/nvim/lua/keymap.lua
+++ env/.config/nvim/lua/keymap.lua
@@ -12,3 +12,10 @@ vim.keymap.set(
 vim.keymap.set({ "n", "v", "x" }, "<leader>p", '"+p', { noremap = true, silent = true, desc = "Paste from clipboard" })
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.config/tmux/tmux-sessionizer<CR>")
+
+vim.keymap.set("n", "<leader>~", ":b#<CR>", { noremap = true, silent = true, desc = "previous buffer" })
+
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
--- env/.config/nvim/lua/plugins/fzf.lua
+++ env/.config/nvim/lua/plugins/fzf.lua
@@ -2,7 +2,16 @@ local fzf = require("fzf-lua")
 
 fzf.setup()
 
-vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "files" })
+vim.keymap.set("n", "<leader>pf", fzf.files, { desc = "files" })
+vim.keymap.set("n", "<leader>pg", fzf.git_files, { desc = "git files" })
 vim.keymap.set("n", "<leader>,", fzf.buffers, { desc = "buffers" })
 vim.keymap.set("n", "<leader>fr", fzf.oldfiles, { desc = "recent files" })
+
+vim.keymap.set("n", "<leader>sp", fzf.live_grep, { desc = "grep files" })
+
 vim.keymap.set("n", "gd", fzf.lsp_definitions, { desc = "goto defs" })
+
+-- my old binds
+vim.keymap.set("n", "<leader>ff", '<cmd>echo "Use pg or pf"<CR>')
+vim.keymap.set("n", "<leader><leader>", '<cmd>echo "Use ,"<CR>')
+vim.keymap.set("n", "<leader>fg", '<cmd>echo "Use sp"<CR>')
