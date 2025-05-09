a8b919b16c96e67b5bcbad67f75d902117234962
Robbo <robbo.clancy@gmail.com>
2025-05-09 18:31:45 +1000

    hi

--- env/.config/nvim/lua/colors.lua
+++ env/.config/nvim/lua/colors.lua
@@ -1,20 +1,31 @@
-vim.api.nvim_create_user_command("Accent", function(opts)
-	if opts.args then
-		vim.g.accent_colour = opts.args
-	else
-		vim.g.accent_auto_cwd_colour = 1
-	end
+-- vim.api.nvim_create_user_command("Accent", function(opts)
+-- 	if opts.args then
+-- 		vim.g.accent_colour = opts.args
+-- 	else
+-- 		vim.g.accent_auto_cwd_colour = 1
+-- 	end
+--
+-- 	vim.g.accent_no_bg = 1
+--
+-- 	vim.opt.termguicolors = false
+-- 	vim.cmd("colorscheme accent")
+--
+-- 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+-- 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#111111" })
+-- 	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#111111" })
+-- 	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "None" })
+-- 	vim.api.nvim_set_hl(0, "TabLineMiniBar", { fg = "#999999" })
+-- end, { nargs = 1 })
+--
+-- vim.cmd("Accent magenta")
 
-	vim.g.accent_no_bg = 1
+require("kanso").setup({
+	undercurl = true,
+	transparent = true,
+	commentStyle = { italic = true },
+	theme = "zen",
+})
 
-	vim.opt.termguicolors = false
-	vim.cmd("colorscheme accent")
+vim.cmd("colorscheme kanso")
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#111111" })
-	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#111111" })
-	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "None" })
-	vim.api.nvim_set_hl(0, "TabLineMiniBar", { fg = "#999999" })
-end, { nargs = 1 })
-
-vim.cmd("Accent magenta")
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#080808" })
--- env/.config/nvim/lua/lsp.lua
+++ env/.config/nvim/lua/lsp.lua
@@ -12,3 +12,44 @@ vim.lsp.config("*", {
 		},
 	}),
 })
+
+--cr - rename
+--ca - code action
+
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "rename" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "code action" })
+
+vim.api.nvim_create_user_command("LspRestart", function()
+	local detach_clients = {}
+	for _, client in ipairs(vim.lsp.get_clients({ bufnr = 0 })) do
+		client:stop(true)
+		if vim.tbl_count(client.attached_buffers) > 0 then
+			detach_clients[client.name] = { client, vim.lsp.get_buffers_by_client_id(client.id) }
+		end
+	end
+	local timer = vim.uv.new_timer()
+	if not timer then
+		return vim.notify("Servers are stopped but havent been restarted")
+	end
+	timer:start(
+		100,
+		50,
+		vim.schedule_wrap(function()
+			for name, client in pairs(detach_clients) do
+				local client_id = vim.lsp.start(client[1].config, { attach = false })
+				if client_id then
+					for _, buf in ipairs(client[2]) do
+						vim.lsp.buf_attach_client(buf, client_id)
+					end
+					vim.notify(name .. ": restarted")
+				end
+				detach_clients[name] = nil
+			end
+			if next(detach_clients) == nil and not timer:is_closing() then
+				timer:close()
+			end
+		end)
+	)
+end, {
+	desc = "Restart all the language clients attached to the current buffer",
+})
