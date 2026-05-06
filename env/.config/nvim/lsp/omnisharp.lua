ee208c2bbdd1b91233d7acad002e7d27edc153ca
Robbo <robbo.clancy@gmail.com>
2026-05-07 01:37:57 +1000

    omnisharp LSP config, treesitter converted to main branch API

--- env/.config/nvim/lsp/omnisharp.lua
+++ env/.config/nvim/lsp/omnisharp.lua
@@ -1,5 +1,22 @@
 return {
-	cmd = { "omnisharp", "--stdio" },
-	filetypes = { "cs" },
-	root_markers = { ".git", "*.sln", "*.csproj" },
+	cmd = {
+		"OmniSharp",
+		"-z",
+		"--hostPID",
+		tostring(vim.fn.getpid()),
+		"DotNet:enablePackageRestore=false",
+		"--encoding",
+		"utf-8",
+		"--languageserver",
+	},
+	filetypes = { "cs", "vb" },
+	root_markers = { "*.sln", "*.csproj" },
+	capabilities = {
+		workspace = {
+			workspaceFolders = false,
+		},
+	},
+	handlers = {
+		["event"] = function() end,
+	},
 }
